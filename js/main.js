8db6fa60c803fb140aa2b3bbc2cae807717c8606
yassersalem14 <salemyasserlallo1415@gmail.com>
2022-09-20 00:38:47 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+menu = document.getElementById("menu");
+links = document.getElementById("links");
+
+menu.addEventListener("click" , function(e){
+  e.preventDefault( )
+    links.classList.toggle("flex")
+    links.classList.toggle("hidden")
+    
+})
